b97bd4d86668abf5360bafdb0f2cb338f0395d12
Julien Roy <julien.s.roy@gmail.com>
2015-03-19 00:52:28 -0700

    ADDING: indicator js

--- framework/js/main.js
+++ framework/js/main.js
@@ -1,3 +1,4 @@
+// this needs refactoring =D
 $(function () {
   'use strict'
 
@@ -7,6 +8,11 @@ $(function () {
 })
 
 var tabSwitch = function (className) {
+  // create indicator on first tab
+  $('.' + className + ':first').append('<figure class="tab--indicator">')
+
+  var indicator = $('.tab--indicator');
+
   // check url for hash and apply switch tab/container state
   if (window.location.hash.length > 0) {
     var hash = window.location.hash,
@@ -18,8 +24,14 @@ var tabSwitch = function (className) {
 
     // check if hash extists within possible tabs
     if ($.inArray(hash, existingLinks) >= 0) {
+      var activeTab = $('[href=' + hash + ']'),
+          index = activeTab.parent().index();
+
       $('.' + className + '.active').removeClass('active')
-      $('[href=' + hash + ']').addClass('active')
+      activeTab.addClass('active')
+
+      // move indicator to correct position
+      indicator.css('transform', 'translateX(' + index * 100 + '%)')
 
       hash = hash.replace('#', '')
       $('[data-page-name=' + hash + ']').fadeIn(177)
@@ -34,11 +46,16 @@ var tabSwitch = function (className) {
     // prevent from triggering on current tab
     if ($(this).hasClass('active')) return
 
-    var linkName = $(this).attr('href').replace('#', '');
+    var activeTab = $('.' + className + '.active'),
+        linkName = $(this).attr('href').replace('#', ''),
+        index = $(this).parent().index();
 
     // toggle tab class
-    $('.' + className + '.active').removeClass('active')
+    activeTab.removeClass('active')
     $(this).addClass('active')
+
+    // move indicator to correct position
+    indicator.css('transform', 'translateX(' + index * 100 + '%)')
 
     // toggle container display
     $('.container').fadeOut(177)
@@ -50,7 +67,7 @@ var ratings = function (className) {
   $('.' + className).each(function () {
     var rating = $(this).data('rating');
 
-    // Assuming the endpoint returns an integer or rounded float
+    // assuming the endpoint returns an integer or rounded float (3.5)
     if (!isNaN(rating) && rating > 0) {
       var star = $(this).find('.ez-star');
 
@@ -74,13 +91,16 @@ var openModal = function (attr) {
   // open modal
   $('[data-action=' + attr + ']').on('click', function () {
     $('body').addClass('noscroll')
-    $('.modal__bg, .modal__container').addClass('active')
+    $('.modal__bg').addClass('active').delay(150).queue(function () {
+      $('.modal__container').addClass('active')
+      $(this).dequeue()
+    })
   })
 
   // close modal
   $('.modal__bg').on('click', function () {
     $('body').removeClass('noscroll')
-    $('.modal__container').removeClass('active').delay(233).queue(function () {
+    $('.modal__container').removeClass('active').delay(300).queue(function () {
       $('.modal__bg').removeClass('active')
       $(this).dequeue()
     })
